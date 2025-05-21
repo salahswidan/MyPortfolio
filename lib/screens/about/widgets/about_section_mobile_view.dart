a79bd0862e6dddacf02ab8ea1f70a664a18a29da
salah <salahswidan212@gmail.com>
2025-05-21 13:48:38 +0300

    first commit

---
+++ lib/screens/about/widgets/about_section_mobile_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'about_me_widget.dart';
+import 'cv.dart';
+import 'view_resume_button.dart';
+
+class AboutSectionMobileView extends StatelessWidget {
+  const AboutSectionMobileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AboutMeWidget(),
+        const SizedBox(height: 80),
+        Row(
+          mainAxisAlignment:
+              kIsWeb ? MainAxisAlignment.spaceAround : MainAxisAlignment.start,
+          children: [
+            ViewResumeButton(),
+
+            if (kIsWeb) Cv(),
+          ],
+        ),
+      ],
+    );
+  }
+}
